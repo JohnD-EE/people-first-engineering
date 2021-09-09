e96665787182f2a270a5da2432c8fc0d4cccd245
John Durrant <johndurrant70@gmail.com>
2021-09-09 16:58:34 +0100

    Spelling and typos

--- src/data/protocols/operationalExcellence.js
+++ src/data/protocols/operationalExcellence.js
@@ -22,7 +22,7 @@ const protocol = {
         {
             name: 'Continuous Improvement',
             paragraphs: [
-                'Bottlenecks and Waste in the SDLC SHOULD be identified and addressed in cycles of Continuous Improvement cycles.',
+                'Bottlenecks and Waste in the SDLC SHOULD be identified and addressed in cycles of Continuous Improvement.',
                 'A cadence of regular Retrospectives SHOULD be scheduled to collaboratively identify common root causes of Bottlenecks and Waste.',
                 'Retrospectives MUST adhere to Psychological Safety protocols.'
             ],
@@ -46,14 +46,14 @@ const protocol = {
             paragraphs: [
                 'Product and Processes SHOULD avoid wasteful complexity. Simplicity in design is key to Operational Excellence.',
                 'Sufficient Hygiene Time SHOULD be allocated to cleansing code issues and process simplification.',
-                'Multiple quality gates and big releases SHOULD be minimised. Little and often releases with automated rollback SHOULD be favoured'
+                'Multiple quality gates and big releases SHOULD be minimised. Little and often releases with automated rollback SHOULD be favoured.'
             ],
         },
         {
             name: 'Infrastructure and Tooling',
             paragraphs: [
                 'Engineers SHOULD be supported in TOIL reduction to automate manual process where appropriate.',
-                'It SHOULD be easy to focus on building and deploying code without unnecessary worries over infrastructure and tools.',
+                'It SHOULD be easy for Engineers to focus on building, testing and deploying code without unnecessary worries over infrastructure and tools.',
                 'Sufficient Hygiene Time SHOULD be allocated to stabilising platform and infrastructure '
             ],
         },
@@ -71,7 +71,7 @@ const protocol = {
                 'Bad relationships MUST be seen as a major source of bottlenecks and waste',
                 'People\'s blindspots, mood variations and natural human limitations MUST be viewed as a majour cause of Operational Imperfections',
                 'Understanding human psychology and navigating difficult relationships SHOULD have a high priotiy when striving for Operational Excellence.',
-                'Coaching and mentoring to improve Engineer Soft SKills SHOULD be provided on a regular cadence.'
+                'Coaching and mentoring to optimise Engineer Soft Skills SHOULD be provided on a regular cadence.'
             ],
         },
     ],
